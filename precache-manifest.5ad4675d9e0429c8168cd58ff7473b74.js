a9acfb04705049f18ce17550903ef963a035094d
NikolasPlatonov <platonov.nikolas@gmail.com>
2020-04-23 15:40:57 +0300

    Updates

---
+++ precache-manifest.5ad4675d9e0429c8168cd58ff7473b74.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6f6dab3e3d2d0275278e311ffe1003",
+    "url": "/zenbit_store/index.html"
+  },
+  {
+    "revision": "7538737117f60e245ad9",
+    "url": "/zenbit_store/static/css/main.393630b0.chunk.css"
+  },
+  {
+    "revision": "a989f3b0cc66146c520a",
+    "url": "/zenbit_store/static/js/2.62003eff.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/zenbit_store/static/js/2.62003eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7538737117f60e245ad9",
+    "url": "/zenbit_store/static/js/main.cf4403d9.chunk.js"
+  },
+  {
+    "revision": "556f1e05a5abe7bd32dc",
+    "url": "/zenbit_store/static/js/runtime-main.a02a1996.js"
+  },
+  {
+    "revision": "b47e2063fdbd5647cf43e6f631c8f6b9",
+    "url": "/zenbit_store/static/media/cycle_arrow_icon.b47e2063.png"
+  },
+  {
+    "revision": "9a614b210d18a33c47cdd8d4cb94d2e2",
+    "url": "/zenbit_store/static/media/house_icon.9a614b21.png"
+  },
+  {
+    "revision": "7d5296ba343f079c185f164d698e296c",
+    "url": "/zenbit_store/static/media/preloader.7d5296ba.gif"
+  },
+  {
+    "revision": "860e1bc4b36cd6c25bd4da0e26ddb94c",
+    "url": "/zenbit_store/static/media/search_icon.860e1bc4.png"
+  },
+  {
+    "revision": "903178f074e474b7051fa3fc20c24702",
+    "url": "/zenbit_store/static/media/shopping-cart.903178f0.png"
+  },
+  {
+    "revision": "a33035bf57c2af2cce2d8cde8213f55e",
+    "url": "/zenbit_store/static/media/wallet_icon.a33035bf.png"
+  }
+]);
